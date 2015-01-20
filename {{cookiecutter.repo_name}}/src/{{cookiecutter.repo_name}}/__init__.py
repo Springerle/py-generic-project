21ac704d47d1c1c7a6ed9a5029178c59d50c4f3b
jhermann <jh@web.de>
2015-01-20 16:43:45 +0100

    basic project files

---
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+"""
+    {{ cookiecutter.repo_name }} package
+"""
+
+__author__ = "{{ cookiecutter.full_name }}"
+__email__ = "{{ cookiecutter.email }}"
+__version__ = "{{ cookiecutter.version }}"
+
+__all__ = []
