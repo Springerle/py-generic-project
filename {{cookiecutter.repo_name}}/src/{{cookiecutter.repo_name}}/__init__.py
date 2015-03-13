e2f15e185fbddef0a8e7a6fd661dc144f944963a
jhermann <jh@web.de>
2015-03-13 16:51:02 +0100

    :arrow_upper_right: more metadata fields, and license blurb

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/__init__.py
@@ -1,10 +1,27 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
+# pylint: disable=bad-whitespace
 """
-    {{ cookiecutter.repo_name }} package
+    {{ cookiecutter.repo_name }} – {{ cookiecutter.short_description }}
+
+    Copyright ©  {{ cookiecutter.year }} {{ cookiecutter.full_name }} <{{ cookiecutter.email }}>
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 """
 
-__author__ = "{{ cookiecutter.full_name }}"
-__email__ = "{{ cookiecutter.email }}"
-__version__ = "{{ cookiecutter.version }}"
+__url__             = "{{ cookiecutter.url }}"
+__version__         = "{{ cookiecutter.version }}"
+__license__         = "Apache 2.0"
+__author__          = "{{ cookiecutter.full_name }}"
+__email__           = "{{ cookiecutter.email }}"
 
 __all__ = []
