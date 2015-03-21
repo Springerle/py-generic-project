c11bcc577cd263c3fc6973bed276c2cfec39583e
jhermann <jh@web.de>
2015-03-21 04:49:17 +0100

    :bug: fix for exclusion of hidden dirs in os.walk

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -112,8 +112,8 @@ def _build_metadata(): # pylint: disable=too-many-locals, too-many-branches
 
     # CLI entry points
     console_scripts = []
-    for path, _, files in os.walk(srcfile('src', package_name)):
-        dirs = [i for i in dirs if not dirs.startswith('.')]
+    for path, dirs, files in os.walk(srcfile('src', package_name)):
+        dirs = [i for i in dirs if not i.startswith('.')]
         if '__main__.py' in files:
             path = path[len(srcfile('src') + os.sep):]
             appname = path.split(os.sep)[-1]
