fa8a852a2083879adb050945abe1176367c0a5dd
jhermann <jh@web.de>
2015-03-22 12:01:46 +0100

    :arrow_upper_right: activated 'print_function' future

--- {{cookiecutter.repo_name}}/src/tests/test_package.py
+++ {{cookiecutter.repo_name}}/src/tests/test_package.py
@@ -15,7 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import, unicode_literals, print_function
 
 from {{ cookiecutter.pkg_name }} import __version__ as version
 
--- {{cookiecutter.repo_name}}/src/{{cookiecutter.pkg_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.pkg_name}}/__init__.py
@@ -17,7 +17,7 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 """
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import, unicode_literals, print_function
 
 __url__             = "{{ cookiecutter.url }}"
 __version__         = "{{ cookiecutter.version }}"
