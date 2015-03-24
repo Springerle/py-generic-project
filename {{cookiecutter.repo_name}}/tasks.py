48c8b47ddc02af440932aca23640214964baff07
jhermann <jh@web.de>
2015-03-24 18:27:13 +0100

    :construction: added tox task, and improved ci task

--- {{cookiecutter.repo_name}}/tasks.py
+++ {{cookiecutter.repo_name}}/tasks.py
@@ -44,7 +44,41 @@ def moar_cookies(mold=''):
     run("git status", echo=True)
 
 
-@task
-def ci(): # pylint: disable=invalid-name
+@task(help={
+    'verbose': "Make 'tox' more talkative",
+    'env-list': "Override list of environments to use (e.g. 'py27,py34')",
+    'opts': "Extra flags for tox",
+})
+def tox(verbose=False, env_list='', opts=''):
+    """Perform multi-environment tests."""
+    snakepits = ['/opt/pyenv/bin'] # TODO: config value
+    cmd = []
+
+    snakepits = [i for i in snakepits if os.path.isdir(i)]
+    if snakepits:
+        cmd += ['PATH="{}:$PATH"'.format(os.pathsep.join(snakepits),)]
+
+    cmd += ['tox']
+    if verbose:
+        cmd += ['-v']
+    if env_list:
+        cmd += ['-e', env_list]
+    cmd += opts
+    cmd += ['2>&1']
+    run(' '.join(cmd), echo=True)
+
+
+@task(help={
+    'pty': "Whether to run commands under a pseudo-tty",
+}) # pylint: disable=invalid-name
+def ci(pty=True):
     """Perform continuous integration tasks."""
-    run("invoke clean --all build --docs test check --reports 2>&1")
+    opts = ['']
+
+    # 'tox' makes no sense in Travis
+    if os.environ.get('TRAVIS', '').lower() == 'true':
+        opts += ['test']
+    else:
+        opts += ['tox']
+
+    run("invoke clean --all build --docs check --reports{} 2>&1".format(' '.join(opts)), echo=True, pty=pty)
