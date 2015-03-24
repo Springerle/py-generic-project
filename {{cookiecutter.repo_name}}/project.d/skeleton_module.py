5422b7b8cdf74c50937f300cf4fb99060552074a
jhermann <jh@web.de>
2015-03-24 23:44:28 +0100

    :zzz: minor module skeleton fixes

--- {{cookiecutter.repo_name}}/project.d/skeleton_module.py
+++ {{cookiecutter.repo_name}}/project.d/skeleton_module.py
@@ -15,12 +15,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import, unicode_literals, print_function
 
 import os
 import sys
 
-# from {{ cookiecutter.pkg_name }} import …
+# from . import …
 
 
 __all__ = []
