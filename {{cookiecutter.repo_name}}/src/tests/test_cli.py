288bb30ac8f708b1ce93bccb65e71a4058664985
jhermann <jh@web.de>
2015-03-24 18:48:49 +0100

    :white_check_mark: backported improved CLI tests from gh-commander

--- {{cookiecutter.repo_name}}/src/tests/test_cli.py
+++ {{cookiecutter.repo_name}}/src/tests/test_cli.py
@@ -2,6 +2,8 @@
 # pylint: disable=wildcard-import, unused-wildcard-import, missing-docstring
 # pylint: disable=redefined-outer-name, no-self-use, bad-continuation
 """ Test '__main__' CLI stub.
+
+    See http://click.pocoo.org/3/testing/
 """
 # Copyright ©  {{ cookiecutter.year }} {{ cookiecutter.full_name }} <{{ cookiecutter.email }}>
 #
@@ -22,40 +24,42 @@ import sys
 
 import sh
 import pytest
+from click.testing import CliRunner
 
 from markers import *
 from {{ cookiecutter.pkg_name }} import __version__ as version
-from {{ cookiecutter.pkg_name }}.__main__ import __app_name__ as cmdname
+from {{ cookiecutter.pkg_name }} import __main__ as main
 
 
-UsageError = sh.ErrorReturnCode_2 # pylint: disable=no-member
+UsageError = sh.ErrorReturnCode_2  # pylint: disable=no-member
 
 
 @pytest.fixture
 def cmd():
     """Command fixture."""
-    return sh.Command(cmdname)
+    return sh.Command(main.__app_name__)
 
 
 @cli
 @integration
 def test_cli_help(cmd):
     result = cmd('--help')
-    lines = result.stdout.splitlines()
+    lines = result.stdout.decode('ascii').splitlines()
 
-    assert cmdname in lines[0].split(), "Command name is reported"
+    assert main.__app_name__ in lines[0].split(), "Command name is reported"
 
 
 @cli
 @integration
 def test_cli_version(cmd):
     result = cmd('--version')
-    reported_version = result.stdout.split()[1]
+    stdout = result.stdout.decode('ascii')
+    reported_version = stdout.split()[1]
     py_version = sys.version.split()[0]
 
-    assert version in result.stdout, "Version string contains version"
+    assert version in stdout, "Version string contains version"
     assert reported_version[:len(version)] == version, "Version is 2nd field"
-    assert py_version in result.stdout, "Python version is reported"
+    assert py_version in stdout, "Python version is reported"
 
 
 @cli
@@ -70,3 +74,21 @@ def test_cli_invalid_option(cmd):
 def test_cli_invalid_sub_command(cmd):
     with pytest.raises(UsageError):
         cmd.sub_command_that_does_not_exist()
+
+
+@cli
+def test_cmd_missing(cmd):
+    runner = CliRunner()
+    result = runner.invoke(main.cli)
+
+    assert result.exit_code == 0
+
+
+@cli
+def test_cmd_help(cmd):
+    runner = CliRunner()
+    result = runner.invoke(main.help_command)
+    word1 = result.output.split()[0]
+
+    assert result.exit_code == 0
+    assert word1 == 'Helpful', "Helpful message is printed"
