f3a0b42fb6fe7ab89846d28a7e49d0abe8993da7
jhermann <jh@web.de>
2015-03-29 12:39:03 +0200

    :twisted_rightwards_arrows: moved 'tox' task to rituals

--- {{cookiecutter.repo_name}}/tasks.py
+++ {{cookiecutter.repo_name}}/tasks.py
@@ -12,7 +12,7 @@ import shutil
 import tempfile
 
 from invoke import run, task
-from rituals.invoke_tasks import * # pylint: disable=redefined-builtin
+from rituals.invoke_tasks import *  # pylint: disable=redefined-builtin
 
 
 @task(name='fresh-cookies',
@@ -49,32 +49,8 @@ def fresh_cookies(mold=''):
 
 
 @task(help={
-    'verbose': "Make 'tox' more talkative",
-    'env-list': "Override list of environments to use (e.g. 'py27,py34')",
-    'opts': "Extra flags for tox",
-})
-def tox(verbose=False, env_list='', opts=''):
-    """Perform multi-environment tests."""
-    snakepits = ['/opt/pyenv/bin'] # TODO: config value
-    cmd = []
-
-    snakepits = [i for i in snakepits if os.path.isdir(i)]
-    if snakepits:
-        cmd += ['PATH="{}:$PATH"'.format(os.pathsep.join(snakepits),)]
-
-    cmd += ['tox']
-    if verbose:
-        cmd += ['-v']
-    if env_list:
-        cmd += ['-e', env_list]
-    cmd += opts
-    cmd += ['2>&1']
-    run(' '.join(cmd), echo=True)
-
-
-@task(help={
     'pty': "Whether to run commands under a pseudo-tty",
-}) # pylint: disable=invalid-name
+})  # pylint: disable=invalid-name
 def ci(pty=True):
     """Perform continuous integration tasks."""
     opts = ['']
