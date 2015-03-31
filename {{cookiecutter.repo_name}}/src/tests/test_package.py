1ac7301836722a9f7b5e489fdec06d315f8de308
jhermann <jh@web.de>
2015-03-31 16:37:07 +0200

    :pencil: no unicode in TODO text (invoke fails)

--- {{cookiecutter.repo_name}}/src/tests/test_package.py
+++ {{cookiecutter.repo_name}}/src/tests/test_package.py
@@ -12,6 +12,6 @@ from {{ cookiecutter.pkg_name }} import __version__ as version
 
 def test_semver():
     """Test a proper semantic version is used."""
-    # TODO Test rules according to PEP440 – Version Identification and Dependency Specification
+    # TODO Test rules according to PEP440 - Version Identification and Dependency Specification
     assert len(version.split('.')) == 3, "Semantic version M.m.µ OK"
     assert all(i.isdigit for i in version.split('.')), "Semantic version parts are numeric"
