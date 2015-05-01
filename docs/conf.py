7debb2834379a780f123529cae8f5af25a587bfc
jhermann <jh@web.de>
2015-05-01 12:41:33 +0200

    :pencil: require only Sphinx 1.2+

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ release = version + '.dev0'
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.3'
+needs_sphinx = '1.2'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
