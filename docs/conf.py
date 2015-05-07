d21fccad9b81e5b8d8c04dd5c2f9f613f7fe3207
jhermann <jh@web.de>
2015-05-07 22:53:03 +0200

    :art: new html_logo

--- docs/conf.py
+++ docs/conf.py
@@ -125,7 +125,7 @@ html_theme_options = dict(
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/img/logo-square-48.png"
+html_logo = "_static/img/springerle-logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
