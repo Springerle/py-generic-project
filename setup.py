4e8ddd1145c88d775419402f630e75d9a83ee74d
jhermann <jh@web.de>
2017-02-28 13:06:08 +0100

    support actually calling the fake setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ project = dict(
     name = 'py-generic-project',
     version = '1.2',
     packages = [],
+    url='https://github.com/Springerle/py-generic-project',
 )
+
+if __name__ == '__main__':
+    import setuptools
+    setuptools.setup(**project)
