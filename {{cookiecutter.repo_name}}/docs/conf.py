cea4d9a4b1daad2b2750fc11449c322778a23015
jhermann <jh@web.de>
2018-12-01 09:33:15 +0100

    exclude a converted README.md from docs

--- {{cookiecutter.repo_name}}/docs/conf.py
+++ {{cookiecutter.repo_name}}/docs/conf.py
@@ -85,7 +85,7 @@ today_fmt = '%Y-%m-%d'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = [
-    "*~",
+    "*~", "README.rst",
 ]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
